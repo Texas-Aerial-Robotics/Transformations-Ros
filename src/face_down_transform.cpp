714b663685e3ffeb5d388a6deb9e1145be229c2f
TAR <eric.emj.johnson@gamil.com>
2018-01-22 19:04:19 -0600

    initial set up transformation node

--- src/face_down_transform.cpp
+++ src/face_down_transform.cpp
@@ -1,13 +1,14 @@
 // Transformation between the facedown camera frame and the drone frame
 
-
+#include <iostream>
 #include <cmath>
 
 
 const float ALPHA = 31.8244;
-const float PIXLES[2] = [640, 480];
+const float PIXLES[2] = {640, 480};
 const float PI = 3.14159;
 
+using namespace std;
 
 double pixel2metric_facedown(double alt, double obj_pix)
 {
@@ -18,5 +19,11 @@ double pixel2metric_facedown(double alt, double obj_pix)
 
   double O_m = (obj_pix/H_p)*H_m;
   return O_m;
+}
+int main()
+{
+	double yPos = pixel2metric_facedown(1, 1);
+	cout << yPos << endl;
 
+	return 0; 
 }

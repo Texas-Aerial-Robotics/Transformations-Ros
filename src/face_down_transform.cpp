890072467a42fc7b384525ca66064b102ea6493e
TAR <texasaerialrobotics@gmail.com>
2018-01-29 18:29:21 -0600

    interfaced transformation with darknet ros

--- src/face_down_transform.cpp
+++ src/face_down_transform.cpp
@@ -1,38 +1,115 @@
 // Transformation between the facedown camera frame and the drone frame
-
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Int8.h"
+#include "darknet_ros_msgs/BoundingBox.h"
+#include "darknet_ros_msgs/BoundingBoxes.h"
 #include <iostream>
+#include <vector>
+#include <string>
 #include <cmath>
 
-
-const float ALPHA = 31.8244;
-const float BETA  = 40.4497;
-const float PIXLES[2] = {640, 480};
-const float PI = 3.14159;
+const double ALPHA = 31.8244;
+const double BETA  = 40.4497;
+const double PIXLES[2] = {640, 480};
+const double PI = 3.14159;
 
 using namespace std;
 
-double pixel2metric_facedown(double alt, double obj_pix)
+void pixel2metric_facedown(double alt, vector<double> obj_pix, vector<double> &O_m)
 {
-
+  
   double H_p  = PIXLES[1];
   double angy = (ALPHA/2.0)*(PI/180.0);
   double H_m  = 2.0*tan(angy)*alt;
 
-  double O_my = (obj_pix/H_p)*H_m;
-
+  double O_my = (obj_pix[1] /H_p)*H_m;
   double W_p  = PIXLES[0];
-  double angx = (beta/2.0)*(PI/180.0);
+  double angx = (BETA/2.0)*(PI/180.0);
+
   double W_m  = 2.0*tan(angx)*alt;
+  double O_mx = (obj_pix[0] /W_p)*W_m;
 
-  double O_mx = (obj_pix/W_p)*W_m;
-  return double O_m[2] = {O_mx, O_my};
+  // O_m[0] = O_mx;
+  // O_m[1] = O_my;
+  ROS_INFO("transformed to x: %f y: %f meters \n", O_mx, O_my); 
 }
 
-int main()
+void chatterCallback(const std_msgs::Int8::ConstPtr& msg)
 {
-  // purely for testing purposes
-	double yPos = pixel2metric_facedown(1, 1);
-	cout << yPos << endl;
-
-	return 0; 
+  int num = msg->data;
+  //ROS_INFO("%d objects found", num);
 }
+void centerPoint(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msgBox)
+{
+  darknet_ros_msgs::BoundingBoxes boxesFound;
+  darknet_ros_msgs::BoundingBox objectBounds;
+  boxesFound = *msgBox;
+  double xCenter;
+  double yCenter;
+  for(int i=0; i < boxesFound.boundingBoxes.size(); i++)
+  {
+    objectBounds = boxesFound.boundingBoxes[i];
+    xCenter = (objectBounds.xmax + objectBounds.xmin)/2;
+    yCenter = (objectBounds.ymax + objectBounds.ymin)/2;
+    std::string objectType = objectBounds.Class;
+    ROS_INFO("%s found x: %f y: %f pixels", objectType.c_str(), xCenter, yCenter); 
+  }
+  double alt = 1;
+  vector<double> obj_pix;
+  obj_pix.push_back(xCenter);
+  obj_pix.push_back(yCenter);
+  vector<double> O_m;
+  pixel2metric_facedown(alt, obj_pix, O_m);
+}
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "listener");
+  
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+  /**
+   * The subscribe() call is how you tell ROS that you want to receive messages
+   * on a given topic.  This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing.  Messages are passed to a callback function, here
+   * called chatterCallback.  subscribe() returns a Subscriber object that you
+   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
+   * object go out of scope, this callback will automatically be unsubscribed from
+   * this topic.
+   *
+   * The second parameter to the subscribe() function is the size of the message
+   * queue.  If messages are arriving faster than they are being processed, this
+   * is the number of messages that will be buffered up before beginning to throw
+   * away the oldest ones.
+   */
+  ros::Subscriber sub2 = n.subscribe("/darknet_ros/bounding_boxes",1 ,centerPoint);
+  ros::Subscriber sub = n.subscribe("/darknet_ros/found_object", 1, chatterCallback);
+
+  /**
+   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+   * callbacks will be called from within this thread (the main one).  ros::spin()
+   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+   */
+  ros::spin();
+
+
+  return 0;
+}
+
+
+

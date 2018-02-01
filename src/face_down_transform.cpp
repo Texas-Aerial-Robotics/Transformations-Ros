7defa95c3135a38dc087600fedfdc24d00de4424
TAR <texasaerialrobotics@gmail.com>
2018-01-31 20:13:51 -0600

    added publisher for roomba position

--- src/face_down_transform.cpp
+++ src/face_down_transform.cpp
@@ -4,7 +4,9 @@
 #include "std_msgs/Int8.h"
 #include "darknet_ros_msgs/BoundingBox.h"
 #include "darknet_ros_msgs/BoundingBoxes.h"
+#include <geometry_msgs/PoseStamped.h>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <cmath>
@@ -15,7 +17,7 @@ const double PIXLES[2] = {640, 480};
 const double PI = 3.14159;
 
 using namespace std;
-
+geometry_msgs::PoseStamped roombaPose;
 void pixel2metric_facedown(double alt, vector<double> obj_pix, vector<double> &O_m)
 {
   
@@ -33,6 +35,9 @@ void pixel2metric_facedown(double alt, vector<double> obj_pix, vector<double> &O
   // O_m[0] = O_mx;
   // O_m[1] = O_my;
   ROS_INFO("transformed to x: %f y: %f meters \n", O_mx, O_my); 
+  roombaPose.pose.position.x = O_my;
+  roombaPose.pose.position.y = O_mx;
+  roombaPose.pose.position.z = 1;
 }
 
 void chatterCallback(const std_msgs::Int8::ConstPtr& msg)
@@ -64,48 +69,33 @@ void centerPoint(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msgBox)
 }
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "listener");
   
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
+  ros::init(argc, argv, "transformations");
+  
   ros::NodeHandle n;
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
+  
   ros::Subscriber sub2 = n.subscribe("/darknet_ros/bounding_boxes",1 ,centerPoint);
   ros::Subscriber sub = n.subscribe("/darknet_ros/found_object", 1, chatterCallback);
+  ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("roombaPose", 1000);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  ros::spin();
+  
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+    chatter_pub.publish(roombaPose);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+
+
+
 
 
   return 0;

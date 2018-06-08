9fcef404bcc2ddfd6c7803655c74670d8a7cdc4d
Mark <markloveland@utexas.edu>
2018-06-07 22:35:38 -0500

    Feet

--- src/side_camera_transform.cpp
+++ src/side_camera_transform.cpp
@@ -15,28 +15,26 @@
 #include "opencv2/core/version.hpp"
 #include "transformations_ros/roombaPoses.h"
 #include "transformations_ros/roombaPose.h"
+#include "sensor_msgs/Imu.h"
 
 int W = 1000; // width of gui
 int gridM = 20; // size of grid in meters
 
-const double PI = 3.14159;
+
 //field of view y
-const double PHI = 31.8244*PI/180;
-//fiield of view x
-const double PHI_X=40*PI/180;
-//rotation angle for y direction
-double  THETA_Y = 0;
-//rotation angle in x direction
-double THETA_X=0;
+const double PHI_Y = (32.751868*M_PI/180);
+//field of view in x
+const double PHI_X = (43.08195*M_PI/180);
+
 const double PIXELS[2] = {640, 480};
 
 
 using namespace std;
-//geometry_msgs::PoseStamped roombaPose;
 nav_msgs::Odometry current_pose;
 std_msgs::Float64 gymOffset;
-//transformations_ros::roombaPoses roombaPositions;
 transformations_ros::roombaPoses roombaPositions1;
+sensor_msgs::Imu IMU;
+
 struct orientation
 {
   float roll;
@@ -45,6 +43,8 @@ struct orientation
   string frame_id;
 };
 orientation CAMPARAMS[5];
+orientation current_orientation;
+
 
 void enu_2_gym(nav_msgs::Odometry current_pose_enu)
 {
@@ -62,6 +62,29 @@ void enu_2_gym(nav_msgs::Odometry current_pose_enu)
   //ROS_INFO("pose gym x: %f y: %f z: %f", current_pose.pose.pose.position.x, current_pose.pose.pose.position.y, current_pose.pose.pose.position.z);
  
 }
+//get current orientation of drone
+void imu_cb(const sensor_msgs::Imu::ConstPtr& msg)
+{
+  IMU = *msg;
+  //cout << IMU << endl;
+
+  float q0 = IMU.orientation.w; //w
+  float q1 = IMU.orientation.x; // x
+  float q2 = IMU.orientation.y; // y
+  float q3 = IMU.orientation.z; // z
+
+  float phi = atan2((2*(q0*q1 + q2*q3)), (1 - 2*(pow(q1,2) + pow(q2,2))) ); // roll
+  float theta = asin( 2*(q0*q2 - q3*q1) ); // pitch
+  float psi = atan2((2*(q0*q3 + q1*q2)), (1 - 2*(pow(q2,2) + pow(q3,2))) ); // yaw
+
+  current_orientation.roll=phi;
+  current_orientation.pitch=theta;
+  current_orientation.yaw=psi;
+
+
+  cout<<"roll :" << phi*(180/M_PI) << " pitch : " << theta*(180/M_PI) << " yaw : " << psi*(180/M_PI) << endl;
+
+}
 //get current position of drone
 void pose_cb(const nav_msgs::Odometry::ConstPtr& msg) 
 {
@@ -74,43 +97,70 @@ void gym_cb(const std_msgs::Float64::ConstPtr& msg)
   gymOffset = *msg;
   //ROS_INFO("current heading: %f", current_heading.data);
 }
-geometry_msgs::PoseStamped pixel2metric_facedown(double alt, vector<double> obj_pix, vector<double> &O_m, orientation camparamOfFrame)
-{ //find puxel of interest in x direction
-  THETA_Y = camparamOfFrame.pitch*(M_PI/180);
-  THETA_Y = camparamOfFrame.roll*(M_PI/180);
-  double T_x=obj_pix[0];
-  double psi_x;
-  double O_mx;
-  double O_my;
-  psi_x=2*abs(T_x/PIXELS[0]*PHI_X-PHI_X/2);
-  if(T_x>PIXELS[0]/2){
-     O_mx=alt*tan(THETA_X+psi_x/2);
-  }else{ 
-    O_mx=alt*tan(THETA_X-psi_x/2);
-  }
-  
-  // find pixel of interest in y direction
-  double T_y=obj_pix[1];
+geometry_msgs::PoseStamped pixel2metric_facedown(double alt, vector<double> obj_pix, orientation camparamOfFrame)
+{
+float theta_x_offset;
+float theta_y_offset;
 
-  double psi;
-  //calculate slice of field of interest
-  psi=2*abs(T_y/PIXELS[1]*PHI-PHI/2);
-  
+  //use drone attitude to modify camera orientation angles
+  if(camparamOfFrame.frame_id=="camera_link"){theta_x_offset=-current_orientation.roll;
+                                            theta_y_offset=-current_orientation.pitch;
 
-  //double O_mx = r_p[0]/3779.527;
-  if(T_y<PIXELS[1]/2){
-    O_my = alt*tan(THETA_Y+psi/2);
-  }else{ 
-    O_my=alt*tan(THETA_Y-psi/2);  
-  }
+  }else if(camparamOfFrame.frame_id=="camera_link2"){theta_x_offset=-current_orientation.pitch;
+                                                    theta_y_offset=current_orientation.roll;
   
-  ROS_INFO("transformed to x: %f y: %f meters \n", O_mx, O_my); 
-  geometry_msgs::PoseStamped roombaPose;
-  roombaPose.pose.position.x = O_mx + current_pose.pose.pose.position.x;
-  roombaPose.pose.position.y = O_my + current_pose.pose.pose.position.y;
-  roombaPose.pose.position.z = alt;
-  //ROS_INFO("roombaPose gym x: %f y: %f z: %f", roombaPose.pose.position.x, roombaPose.pose.position.y, roombaPose.pose.position.z);
-  return roombaPose;
+  }else if(camparamOfFrame.frame_id== "camera_link3"){theta_x_offset=current_orientation.roll;
+                                                      theta_y_offset=current_orientation.pitch;                                                
+  }else if(camparamOfFrame.frame_id== "camera_link4"){theta_x_offset=current_orientation.pitch;
+                                                      theta_y_offset=-current_orientation.roll;
+  }else if(camparamOfFrame.frame_id=="camera_link5"){theta_x_offset=-current_orientation.roll;
+                                                    theta_y_offset=-current_orientation.pitch;}
+    
+
+  
+  //find pixel of interest in x direction
+	double THETA_Y = camparamOfFrame.pitch*(M_PI/180)+theta_y_offset;
+	double THETA_X = theta_x_offset;
+	double T_x=obj_pix[0];
+	double psi_x;
+	double O_mx;
+
+	psi_x=2*abs(T_x/PIXELS[0]*PHI_X-PHI_X/2);
+	if(T_x>PIXELS[0]/2){
+	 O_mx=alt*tan(THETA_X+psi_x/2);
+	}else{ 
+	O_mx=alt*tan(THETA_X-psi_x/2);
+	}
+  
+	// find pixel of interest in y direction
+	double T_y=obj_pix[1];
+	double O_my;
+	double psi_y;
+	//calculate slice of field of interest
+	psi_y=2*abs(T_y/PIXELS[1]*PHI_Y-PHI_Y/2);
+
+
+	//double O_mx = r_p[0]/3779.527;
+	if(T_y<PIXELS[1]/2){
+	O_my = alt*tan(THETA_Y+psi_y/2);
+	}else{ 
+	O_my=alt*tan(THETA_Y-psi_y/2);  
+	}
+  
+	ROS_INFO("transformed to x: %f y: %f meters \n", O_mx, O_my); 
+	
+	//put transformed point in drone reference frame
+	float GYM_OFFSET = gymOffset.data;
+	float deg2rad = (M_PI/180);
+	float O_mx_drone = O_mx*cos(camparamOfFrame.yaw*deg2rad) - O_my*sin(camparamOfFrame.yaw*deg2rad);
+	float O_my_drone = O_mx*sin(camparamOfFrame.yaw*deg2rad) + O_my*cos(camparamOfFrame.yaw*deg2rad);
+
+	geometry_msgs::PoseStamped roombaPose;
+	roombaPose.pose.position.x = O_mx_drone + current_pose.pose.pose.position.x;
+	roombaPose.pose.position.y = O_my_drone + current_pose.pose.pose.position.y;
+	roombaPose.pose.position.z = 0;
+	ROS_INFO("roombaPose gym x: %f y: %f z: %f", roombaPose.pose.position.x, roombaPose.pose.position.y, roombaPose.pose.position.z);
+	return roombaPose;
  
 }
 
@@ -162,9 +212,9 @@ void centerPoint(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msgBox)
     vector<double> obj_pix;
     obj_pix.push_back(xCenter);
     obj_pix.push_back(yCenter);
-    vector<double> O_m;
+    
 
-    roombaPoseMsg.roombaPose = pixel2metric_facedown(alt, obj_pix, O_m, frameParam); 
+    roombaPoseMsg.roombaPose = pixel2metric_facedown(alt, obj_pix, frameParam); 
     roombaPoseMsg.roombaPose.header.stamp = boxesFound.header.stamp;
     roombaPoseMsg.roombaPose.header.frame_id = frameParam.frame_id;
     roombaPositions.roombaPoses.push_back(roombaPoseMsg);
@@ -181,7 +231,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "transformations");
   
   ros::NodeHandle n;
-  ros::Subscriber currentPos = n.subscribe<nav_msgs::Odometry>("mavros/global_position/local", 10, pose_cb);
+  ros::Subscriber currentPos = n.subscribe<nav_msgs::Odometry>("mavros/global_position/local", 1, pose_cb);
+  ros::Subscriber currentIMU = n.subscribe<sensor_msgs::Imu>("/mavros/imu/data", 1, imu_cb);
   ros::Subscriber sub2 = n.subscribe("/darknet_ros/bounding_boxes",1 ,centerPoint);
   ros::Subscriber sub = n.subscribe("/darknet_ros/found_object", 1, chatterCallback);
   ros::Subscriber gym_offset_sub = n.subscribe("/gymOffset", 1, gym_cb);
@@ -197,7 +248,7 @@ int main(int argc, char **argv)
   }else{
     ros::param::get("/camera_params/cameras/cam1/orientation/roll", cam1.roll);
     ros::param::get("/camera_params/cameras/cam1/orientation/pitch", cam1.pitch);
-    ros::param::get("/camera_params/cameras/cam1/orientation/roll", cam1.yaw);
+    ros::param::get("/camera_params/cameras/cam1/orientation/yaw", cam1.yaw);
     ros::param::get("/camera_params/cameras/cam1/frame_id", cam1.frame_id);
     ROS_INFO("Camera 1 Parameters loaded");
   }
@@ -207,7 +258,7 @@ int main(int argc, char **argv)
   }else{
     ros::param::get("/camera_params/cameras/cam2/orientation/roll", cam2.roll);
     ros::param::get("/camera_params/cameras/cam2/orientation/pitch", cam2.pitch);
-    ros::param::get("/camera_params/cameras/cam2/orientation/roll", cam2.yaw);
+    ros::param::get("/camera_params/cameras/cam2/orientation/yaw", cam2.yaw);
     ros::param::get("/camera_params/cameras/cam2/frame_id", cam2.frame_id);
     ROS_INFO("Camera 2 Parameters loaded");
   }
@@ -217,7 +268,7 @@ int main(int argc, char **argv)
   }else{
     ros::param::get("/camera_params/cameras/cam3/orientation/roll", cam3.roll);
     ros::param::get("/camera_params/cameras/cam3/orientation/pitch", cam3.pitch);
-    ros::param::get("/camera_params/cameras/cam3/orientation/roll", cam3.yaw);
+    ros::param::get("/camera_params/cameras/cam3/orientation/yaw", cam3.yaw);
     ros::param::get("/camera_params/cameras/cam3/frame_id", cam3.frame_id);
     ROS_INFO("Camera 3 Parameters loaded");
   }
@@ -227,7 +278,7 @@ int main(int argc, char **argv)
   }else{
     ros::param::get("/camera_params/cameras/cam4/orientation/roll", cam4.roll);
     ros::param::get("/camera_params/cameras/cam4/orientation/pitch", cam4.pitch);
-    ros::param::get("/camera_params/cameras/cam4/orientation/roll", cam4.yaw);
+    ros::param::get("/camera_params/cameras/cam4/orientation/yaw", cam4.yaw);
     ros::param::get("/camera_params/cameras/cam4/frame_id", cam4.frame_id);
     ROS_INFO("Camera 4 Parameters loaded");
   }
@@ -237,7 +288,7 @@ int main(int argc, char **argv)
   }else{
     ros::param::get("/camera_params/cameras/cam5/orientation/roll", cam5.roll);
     ros::param::get("/camera_params/cameras/cam5/orientation/pitch", cam5.pitch);
-    ros::param::get("/camera_params/cameras/cam5/orientation/roll", cam5.yaw);
+    ros::param::get("/camera_params/cameras/cam5/orientation/yaw", cam5.yaw);
     ros::param::get("/camera_params/cameras/cam5/frame_id", cam5.frame_id);
     ROS_INFO("Camera 5 Parameters loaded");
   }

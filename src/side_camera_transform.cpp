b8c12b77b7207c655210002aaae5346ee1300021
eric <eric.emj.johnson@gmail.com>
2019-01-28 09:44:44 -0600

    working on making package more robust and generic

--- src/side_camera_transform.cpp
+++ src/side_camera_transform.cpp
@@ -6,19 +6,18 @@
 #include "darknet_ros_msgs/BoundingBox.h"
 #include "darknet_ros_msgs/BoundingBoxes.h"
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PointStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <string>
 #include <cmath>
+#include <sensor_msgs/Image.h>
 
-#include "transformations_ros/roombaPoses.h"
-#include "transformations_ros/roombaPose.h"
+#include "transformations_ros/objects.h"
+#include "transformations_ros/object.h"
 #include "sensor_msgs/Imu.h"
-
-int W = 1000; // width of gui
-int gridM = 20; // size of grid in meters
 
 
 //field of view y
@@ -26,15 +25,17 @@ const double PHI_Y = (32.751868*M_PI/180);
 //field of view in x
 const double PHI_X = (43.08195*M_PI/180);
 
-const double PIXELS[2] = {640, 480};
+double PIXELS[2] = {640, 480};
 
 
 using namespace std;
-nav_msgs::Odometry current_pose;
-std_msgs::Float64 gymOffset;
-transformations_ros::roombaPoses roombaPositions1;
+nav_msgs::Odometry current_pose_g;
+std_msgs::Float64 local_offset_msg;
+transformations_ros::objects roombaPositions1;
 sensor_msgs::Imu IMU;
 std_msgs::Float64 current_heading;
+float current_heading_g;
+float local_offset_g = 0;
 
 struct orientation
 {
@@ -43,23 +44,40 @@ struct orientation
   float yaw;
   string frame_id;
 };
-orientation CAMPARAMS[5];
+std::vector<orientation> cam_params_g;
 orientation current_orientation;
 
-
-void enu_2_gym(nav_msgs::Odometry current_pose_enu)
+void image_cb(const sensor_msgs::Image::ConstPtr& msg)
 {
-  float GYM_OFFSET = gymOffset.data;
+ sensor_msgs::Image detection_image;
+ detection_image = *msg;
+ PIXELS[0] = detection_image.height;
+ PIXELS[1] = detection_image.width;
+}
+void enu_2_local(nav_msgs::Odometry current_pose_enu)
+{
   float x = current_pose_enu.pose.pose.position.x;
   float y = current_pose_enu.pose.pose.position.y;
   float z = current_pose_enu.pose.pose.position.z;
   float deg2rad = (M_PI/180);
-  float X = x*cos(GYM_OFFSET*deg2rad) - y*sin(GYM_OFFSET*deg2rad);
-  float Y = x*sin(GYM_OFFSET*deg2rad) + y*cos(GYM_OFFSET*deg2rad);
+  float X = x*cos(local_offset_g*deg2rad) - y*sin(local_offset_g*deg2rad);
+  float Y = x*sin(local_offset_g*deg2rad) + y*cos(local_offset_g*deg2rad);
   float Z = z;
-  current_pose.pose.pose.position.x = X;
-  current_pose.pose.pose.position.y = Y;
-  current_pose.pose.pose.position.z = Z;
+  //ROS_INFO("Local position %f %f %f",X, Y, Z);
+}
+void enu_2_gym(nav_msgs::Odometry current_pose_enu)
+{
+  float local_offset = local_offset_msg.data;
+  float x = current_pose_enu.pose.pose.position.x;
+  float y = current_pose_enu.pose.pose.position.y;
+  float z = current_pose_enu.pose.pose.position.z;
+  float deg2rad = (M_PI/180);
+  float X = x*cos(local_offset*deg2rad) - y*sin(local_offset*deg2rad);
+  float Y = x*sin(local_offset*deg2rad) + y*cos(local_offset*deg2rad);
+  float Z = z;
+  current_pose_g.pose.pose.position.x = X;
+  current_pose_g.pose.pose.position.y = Y;
+  current_pose_g.pose.pose.position.z = Z;
 
 
 }
@@ -72,7 +90,6 @@ void heading_cb(const std_msgs::Float64::ConstPtr& msg)
 void imu_cb(const sensor_msgs::Imu::ConstPtr& msg)
 {
   IMU = *msg;
-  //cout << IMU << endl;
 
   float q0 = IMU.orientation.w; //w
   float q1 = IMU.orientation.x; // x
@@ -87,27 +104,35 @@ void imu_cb(const sensor_msgs::Imu::ConstPtr& msg)
   current_orientation.pitch=theta;
   current_orientation.yaw=psi;
 
-
-  //cout<<"roll :" << phi*(180/M_PI) << " pitch : " << theta*(180/M_PI) << " yaw : " << psi*(180/M_PI) << endl;
-
 }
 //get current position of drone
 void pose_cb(const nav_msgs::Odometry::ConstPtr& msg)
 {
   nav_msgs::Odometry current_pose_enu = *msg;
   enu_2_gym(current_pose_enu);
-  //ROS_INFO("pose enu x: %f y: %f z: %f", current_pose_enu.pose.pose.position.x, current_pose_enu.pose.pose.position.y, current_pose.pose.pose.position.z);
+
+  float q0 = current_pose_enu.pose.pose.orientation.w;
+  float q1 = current_pose_enu.pose.pose.orientation.x;
+  float q2 = current_pose_enu.pose.pose.orientation.y;
+  float q3 = current_pose_enu.pose.pose.orientation.z;
+  float psi = atan2((2*(q0*q3 + q1*q2)), (1 - 2*(pow(q2,2) + pow(q3,2))) );
+  //ROS_INFO("Current Heading %f ENU", psi*(180/M_PI));
+  //Heading is in ENU
+  current_heading_g = psi*(180/M_PI) - local_offset_g;
+
+  //ROS_INFO("pose enu x: %f y: %f z: %f", current_pose_enu.pose.pose.position.x, current_pose_enu.pose.pose.position.y, current_pose_g.pose.pose.position.z);
 }
-void gym_cb(const std_msgs::Float64::ConstPtr& msg)
+void local_offset_cb(const std_msgs::Float64::ConstPtr& msg)
 {
-  gymOffset = *msg;
+  local_offset_msg = *msg;
   //ROS_INFO("current heading: %f", current_heading.data);
 }
-geometry_msgs::PoseStamped pixel2metric_facedown(double alt, vector<double> obj_pix, orientation camparamOfFrame)
+geometry_msgs::PointStamped pixel2metric(double alt, vector<double> obj_pix, orientation camparamOfFrame)
 {
-float theta_x_offset;
-float theta_y_offset;
+  float theta_x_offset;
+  float theta_y_offset;
 
+  //TODO: make this mathematic
   //use drone attitude to modify camera orientation angles
   if(camparamOfFrame.frame_id=="camera_link"){theta_x_offset=-current_orientation.roll;
                                             theta_y_offset=-current_orientation.pitch;
@@ -121,8 +146,7 @@ float theta_y_offset;
                                                       theta_y_offset=-current_orientation.roll;
   }else if(camparamOfFrame.frame_id=="camera_link5"){theta_x_offset=-current_orientation.roll;
                                                     theta_y_offset=-current_orientation.pitch;}
-
-
+  //END TODO
 
   //find pixel of interest in x direction
 	double THETA_Y = camparamOfFrame.pitch*(M_PI/180)+theta_y_offset;
@@ -162,23 +186,22 @@ float theta_y_offset;
 
 	ROS_INFO("transformed to x: %f y: %f meters \n", O_mx, O_my);
 
-
 	//put transformed point in drone reference frame
-	float GYM_OFFSET = gymOffset.data;
+	float local_offset = local_offset_msg.data;
 	float deg2rad = (M_PI/180);
 	float O_mx_drone = O_mx*cos(camparamOfFrame.yaw*deg2rad) - O_my*sin(camparamOfFrame.yaw*deg2rad);
 	float O_my_drone = O_mx*sin(camparamOfFrame.yaw*deg2rad) + O_my*cos(camparamOfFrame.yaw*deg2rad);
   //ROS_INFO("cam params to x: %f y: %f meters \n", O_mx_drone, O_my_drone);
   //put point in gym reference frame
-  float X = O_mx_drone*cos(-(current_heading.data-GYM_OFFSET )*deg2rad) - O_my_drone*sin(-(current_heading.data-GYM_OFFSET)*deg2rad);
-  float Y = O_mx_drone*sin(-(current_heading.data-GYM_OFFSET )*deg2rad) + O_my_drone*cos(-(current_heading.data-GYM_OFFSET)*deg2rad);
+  float X = O_mx_drone*cos(-(current_heading_g-local_offset )*deg2rad) - O_my_drone*sin(-(current_heading_g-local_offset)*deg2rad);
+  float Y = O_mx_drone*sin(-(current_heading_g-local_offset )*deg2rad) + O_my_drone*cos(-(current_heading_g-local_offset)*deg2rad);
 
-	geometry_msgs::PoseStamped roombaPose;
-	roombaPose.pose.position.x = X + current_pose.pose.pose.position.x;
-	roombaPose.pose.position.y = Y + current_pose.pose.pose.position.y;
-	roombaPose.pose.position.z = 0;
-  //ROS_INFO("pose gym x: %f y: %f z: %f", current_pose.pose.pose.position.x, current_pose.pose.pose.position.y, current_pose.pose.pose.position.z);
-	ROS_INFO("roombaPose gym x: %f y: %f z: %f", roombaPose.pose.position.x, roombaPose.pose.position.y, roombaPose.pose.position.z);
+	geometry_msgs::PointStamped roombaPose;
+	roombaPose.point.x = X + current_pose_g.pose.pose.position.x;
+	roombaPose.point.y = Y + current_pose_g.pose.pose.position.y;
+	roombaPose.point.z = 0;
+  //ROS_INFO("pose gym x: %f y: %f z: %f", current_pose_g.pose.pose.position.x, current_pose_g.pose.pose.position.y, current_pose_g.pose.pose.position.z);
+	ROS_INFO("roombaPose gym x: %f y: %f z: %f", roombaPose.point.x, roombaPose.point.y, roombaPose.point.z);
 	return roombaPose;
 
 }
@@ -193,14 +216,13 @@ void centerPoint(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msgBox)
   darknet_ros_msgs::BoundingBoxes boxesFound;
   darknet_ros_msgs::BoundingBox objectBounds;
   boxesFound = *msgBox;
-  //cout << boxesFound << endl;
   // get camera mounting angles
   orientation frameParam;
-  for(int i=0; i<=5; i++)
+  for(int i=0; i<cam_params_g.size(); i++)
   {
-    if(boxesFound.header.frame_id == CAMPARAMS[i].frame_id)
+    if(boxesFound.image_header.frame_id == cam_params_g[i].frame_id)
     {
-      frameParam = CAMPARAMS[i];
+      frameParam = cam_params_g[i];
       break;
     }
   }
@@ -209,16 +231,16 @@ void centerPoint(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msgBox)
 
   double xCenter;
   double yCenter;
-  double alt = current_pose.pose.pose.position.z;
+  double alt = current_pose_g.pose.pose.position.z;
   int numDetections = boxesFound.bounding_boxes.size();
-  transformations_ros::roombaPoses roombaPositions;
-  transformations_ros::roombaPose roombaPoseMsg;
+  transformations_ros::objects roombaPositions;
+  transformations_ros::object roombaPoseMsg;
 
   //apply transformation to each detection and repackage for stratnode
   for(int i=0; i < boxesFound.bounding_boxes.size(); i++)
   {
     //safety incase there are too many detections
-    if(i>10)
+    if(i>100)
     {
       break;
     }
@@ -232,13 +254,12 @@ void centerPoint(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msgBox)
     obj_pix.push_back(xCenter);
     obj_pix.push_back(yCenter);
 
-
-    roombaPoseMsg.roombaPose = pixel2metric_facedown(alt, obj_pix, frameParam);
-    roombaPoseMsg.roombaPose.header.stamp = boxesFound.header.stamp;
-    roombaPoseMsg.roombaPose.header.frame_id = frameParam.frame_id;
-    roombaPositions.roombaPoses.push_back(roombaPoseMsg);
+    roombaPoseMsg.Class = boxesFound.bounding_boxes[i].Class;
+    roombaPoseMsg.position = pixel2metric(alt, obj_pix, frameParam);
+    roombaPoseMsg.position.header.stamp = boxesFound.header.stamp;
+    roombaPoseMsg.position.header.frame_id = frameParam.frame_id;
+    roombaPositions.objects.push_back(roombaPoseMsg);
   }
-  //cout << roombaPositions << endl;
   roombaPositions1 = roombaPositions;
 
 
@@ -250,87 +271,53 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "transformations");
 
   ros::NodeHandle n;
-  ros::Subscriber currentPos = n.subscribe<nav_msgs::Odometry>("mavros/global_position/local", 1, pose_cb);
+  ros::Subscriber currentPos = n.subscribe<nav_msgs::Odometry>("/mavros/local_position/odom", 1, pose_cb);
   ros::Subscriber currentHeading = n.subscribe<std_msgs::Float64>("mavros/global_position/compass_hdg", 1, heading_cb);
   ros::Subscriber currentIMU = n.subscribe<sensor_msgs::Imu>("/mavros/imu/data", 1, imu_cb);
+  ros::Subscriber detectionImage = n.subscribe("/darknet_ros/detection_image", 1, image_cb);
   ros::Subscriber sub2 = n.subscribe("/darknet_ros/bounding_boxes",1 ,centerPoint);
   ros::Subscriber sub = n.subscribe("/darknet_ros/found_object", 1, chatterCallback);
-  ros::Subscriber gym_offset_sub = n.subscribe("/gymOffset", 1, gym_cb);
-  ros::Publisher chatter_pub = n.advertise<transformations_ros::roombaPoses>("roombaPoses", 1000);
+  ros::Subscriber local_offset_sub = n.subscribe("/controlnode/local_offset", 1, local_offset_cb);
+  ros::Publisher chatter_pub = n.advertise<transformations_ros::objects>("objects", 10);
 
-
-  orientation cam1, cam2, cam3, cam4, cam5;
-
-  // get params
-  if (!n.hasParam("/camera_params/cameras/cam1/orientation/roll"))
+  int numOfCams = 0;
+  //figure out how many cameras are on aircraft
+  string prefix = "/camera_params/cameras/";
+  if (!n.hasParam((prefix + "numOfCams").c_str()))
   {
-    ROS_INFO("No param named 'cam1'");
+    ROS_INFO("No param named numOfCams");
   }else{
-    ros::param::get("/camera_params/cameras/cam1/orientation/roll", cam1.roll);
-    ros::param::get("/camera_params/cameras/cam1/orientation/pitch", cam1.pitch);
-    ros::param::get("/camera_params/cameras/cam1/orientation/yaw", cam1.yaw);
-    ros::param::get("/camera_params/cameras/cam1/frame_id", cam1.frame_id);
-    ROS_INFO("Camera 1 Parameters loaded");
-  }
-   if (!n.hasParam("/camera_params/cameras/cam2/orientation/roll"))
-  {
-    ROS_INFO("No param named 'cam2'");
-  }else{
-    ros::param::get("/camera_params/cameras/cam2/orientation/roll", cam2.roll);
-    ros::param::get("/camera_params/cameras/cam2/orientation/pitch", cam2.pitch);
-    ros::param::get("/camera_params/cameras/cam2/orientation/yaw", cam2.yaw);
-    ros::param::get("/camera_params/cameras/cam2/frame_id", cam2.frame_id);
-    ROS_INFO("Camera 2 Parameters loaded");
-  }
-   if (!n.hasParam("/camera_params/cameras/cam3/orientation/roll"))
-  {
-    ROS_INFO("No param named 'cam3'");
-  }else{
-    ros::param::get("/camera_params/cameras/cam3/orientation/roll", cam3.roll);
-    ros::param::get("/camera_params/cameras/cam3/orientation/pitch", cam3.pitch);
-    ros::param::get("/camera_params/cameras/cam3/orientation/yaw", cam3.yaw);
-    ros::param::get("/camera_params/cameras/cam3/frame_id", cam3.frame_id);
-    ROS_INFO("Camera 3 Parameters loaded");
-  }
-   if (!n.hasParam("/camera_params/cameras/cam4/orientation/roll"))
-  {
-    ROS_INFO("No param named 'cam4'");
-  }else{
-    ros::param::get("/camera_params/cameras/cam4/orientation/roll", cam4.roll);
-    ros::param::get("/camera_params/cameras/cam4/orientation/pitch", cam4.pitch);
-    ros::param::get("/camera_params/cameras/cam4/orientation/yaw", cam4.yaw);
-    ros::param::get("/camera_params/cameras/cam4/frame_id", cam4.frame_id);
-    ROS_INFO("Camera 4 Parameters loaded");
-  }
-   if (!n.hasParam("/camera_params/cameras/cam5/orientation/roll"))
-  {
-    ROS_INFO("No param named 'cam5'");
-  }else{
-    ros::param::get("/camera_params/cameras/cam5/orientation/roll", cam5.roll);
-    ros::param::get("/camera_params/cameras/cam5/orientation/pitch", cam5.pitch);
-    ros::param::get("/camera_params/cameras/cam5/orientation/yaw", cam5.yaw);
-    ros::param::get("/camera_params/cameras/cam5/frame_id", cam5.frame_id);
-    ROS_INFO("Camera 5 Parameters loaded");
-  }
-   if (!n.hasParam("/camera_params/gymOffSet"))
-  {
-    ROS_INFO("No param named 'gymOffSet'");
-  }else{
-    ros::param::get("/camera_params/gymOffSet", gymOffset.data);
-    ROS_INFO("gymOffset Parameter loaded");
+    ros::param::get((prefix + "numOfCams").c_str(), numOfCams);
+    ROS_INFO("%d cameras on aircraft", numOfCams);
   }
 
-  CAMPARAMS[0] = cam1;
-  CAMPARAMS[1] = cam2;
-  CAMPARAMS[2] = cam3;
-  CAMPARAMS[3] = cam4;
-  CAMPARAMS[4] = cam5;
+  //load the camera intrinsics 
+  string param_path;
+  orientation cam;
+  for( int i=1; i<=numOfCams; i++)
+  {
+    param_path = "";
+    param_path = prefix + "cam" + std::to_string(i);
+    // get params
+    if (!n.hasParam( (param_path ).c_str() ) )
+    {
+      ROS_ERROR("No param named %s", param_path.c_str());
+      return -1;
+    }else{
+      ros::param::get((param_path + "/orientation/roll").c_str() , cam.roll);
+      ros::param::get((param_path + "/orientation/pitch").c_str() , cam.pitch);
+      ros::param::get((param_path + "/orientation/yaw").c_str() , cam.yaw);
+      ros::param::get((param_path + "/frame_id").c_str() , cam.frame_id);
+      cam_params_g.push_back(cam);
 
+      ROS_INFO("%s Parameters loaded", param_path.c_str());
+    }
+  }
+  
   ROS_INFO("Parameters loaded");
   ROS_INFO("Node Started");
 
   ros::Rate loop_rate(10);
-
   while (ros::ok())
   {
 
@@ -341,12 +328,6 @@ int main(int argc, char **argv)
     loop_rate.sleep();
 
   }
-
-
-
-
-
-
 
   return 0;
 }
